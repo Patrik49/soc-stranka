70f0b2c4eab6d6b1fc04cf6c2254da85c2e593c1
Patrik49 <patricius5553@gmail.com>
2025-12-05 10:54:29 +0100

    zly(chybny index - a tag bol presunutý kvoli buttonu, aby hrefko nebralo cele, DOROBIT

---
+++ script/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Nájde všetky tlačidlá s triedou "pridat"
+    const buttons = document.querySelectorAll('.pridat');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Odložíme si pôvodný text tlačidla
+            const originalText = this.innerText;
+
+            // Pridáme triedu pre zelenú farbu
+            this.classList.add('uspech');
+            // Zmeníme text pre lepšiu odozvu
+            this.innerText = "Pridané ✔";
+
+            // Po 1.5 sekunde (1500 ms) vrátime všetko do pôvodného stavu
+            setTimeout(() => {
+                this.classList.remove('uspech');
+                this.innerText = originalText;
+            }, 1500);
+        });
+    });
+});
